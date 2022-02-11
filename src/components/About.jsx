f415b506a55d7e7782662c70a7483f81e35cbbf6
taherahmed14 <taherahmed.sj.95@gmail.com>
2022-02-11 11:50:34 +0530

    resume update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -31,7 +31,7 @@ export const AboutMe = ({ skillsRef, aboutRef }) => {
                         A <strong style={{color:"#6889FF"}}>Full Stack Web Developer</strong> based in India. A creative coder who specializes in MERN stack. I make it my mission to design pixel-perfect websites or applications with optimized code that run blazing fast.
                     </Text>
 
-                    <Link margin="auto" className='link' href="https://drive.google.com/file/d/1ol6jfIdTra58KKPAd7P7q8H4LG_Oy7HP/view?usp=sharing" target='_blank'>
+                    <Link margin="auto" className='link' href="https://drive.google.com/drive/u/0/folders/182tGyHK915BsS30Fo4dJxibT52ueGqwT" target='_blank'>
                         <span>Resume</span>
                         <div class="wave"></div>
                     </Link>
