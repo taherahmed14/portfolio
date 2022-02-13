0123e1c0e6a757ba43ac2a0a1ed381c77a3fad99
taherahmed14 <taherahmed.sj.95@gmail.com>
2022-02-13 20:30:08 +0530

    Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,27 +2,27 @@ import { Center, Container, Heading, Box, SimpleGrid, Image, Link, Text } from "
 import "./DownArrow.css";
 import { useMediaQuery } from '@chakra-ui/react';
 import "./Video.css";
-import { useState } from "react";
-import { DemoVideoOne, DemoVideoThree } from "./DemoVideo";
+// import { useState } from "react";
+// import { DemoVideoOne, DemoVideoThree } from "./DemoVideo";
 import "./Projects.css";
 
 export const Projects = ({ projectRef }) => {
     const [isLargerThan1200] = useMediaQuery('(min-width: 1200px)');
-    const [showVideoOne, setShowVideoOne] = useState(false);
+    // const [showVideoOne, setShowVideoOne] = useState(false);
     // const [showVideoTwo, setShowVideoTwo] = useState(false);
-    const [showVideoThree, setShowVideoThree] = useState(false);
+    // const [showVideoThree, setShowVideoThree] = useState(false);
 
-    const closeDemoVideoOne = () => {
-        setShowVideoOne(false);
-    }
+    // const closeDemoVideoOne = () => {
+    //     setShowVideoOne(false);
+    // }
 
     // const closeDemoVideoTwo = () => {
     //     setShowVideoTwo(false);
     // }
 
-    const closeDemoVideoThree = () => {
-        setShowVideoThree(false);
-    }
+    // const closeDemoVideoThree = () => {
+    //     setShowVideoThree(false);
+    // }
 
     return(
         <> 
@@ -33,7 +33,7 @@ export const Projects = ({ projectRef }) => {
             </Center>
             {/* minW={300} borderRadius='20px' _hover={{ boxShadow: '0 0 8px #6889FF' }} */}
             <Center mt={60}>
-                <SimpleGrid columns={2} spacing={30}>
+                <SimpleGrid columns={3} spacing={30}>
                     <Container className="projectCard">
                         <Box borderRadius='20px'>
                             <Image borderRadius='20px 20px 0px 0px' minW={300} height={300} src="groww-project.png" alt="groww-project" />
@@ -64,58 +64,16 @@ export const Projects = ({ projectRef }) => {
                                     _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
                                     >Website
                                 </Link>    
-                                <Link textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
+                                {/* <Link textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
                                     border='2px solid #C668FF'
                                     _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
                                     onClick={() => setShowVideoOne(true)}>
                                     Demo
-                                </Link>
+                                </Link> */}
                             </Box>
                         </Box>
-                        {showVideoOne ? <DemoVideoOne closeDemoVideoOne={closeDemoVideoOne} /> : null}
+                        {/* {showVideoOne ? <DemoVideoOne closeDemoVideoOne={closeDemoVideoOne} /> : null} */}
                     </Container>
-
-                    {/* <Container className="projectCard">
-                        <Box borderRadius='20px'>
-                            <Image borderRadius='20px 20px 0px 0px' minW={300} height={300} src="groww-project.png" alt="adidas-project" />
-                        </Box>
-
-                        <Box mt={-4} p={10} className="projectButton">
-                            <Box className="projectDescBox">
-                                <Text className="projectDescText">A full-stack clone of groww application developed by 6 members in time span of 5 days.
-                                    We have added features like filter products, view product graph, set SIP amount, OTP mail validation etc. 
-                                </Text>
-                            </Box>
-
-                            <Box display='flex' className="techStackBox" justifyContent='center'>
-                                <Text className="techStack">HTML</Text>
-                                <Text className="techStack">CSS</Text>
-                                <Text className="techStack">JS</Text>
-                                <Text className="techStack">Node</Text>
-                                <Text className="techStack">MongoDb</Text>
-                            </Box>
-
-                            <Box className="buttonBox">
-                                <Link href="https://github.com/taherahmed14/Groww_Full-Stack_Project" textDecoration='none' fontSize='14px' ml={20} color='#C668FF' p={8} 
-                                    border='2px solid #C668FF' target='_blank'
-                                    _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
-                                    >Code
-                                </Link>
-                                <Link href="https://groww-project.herokuapp.com/home" textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
-                                    border='2px solid #C668FF' target='_blank'
-                                    _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
-                                    >Website
-                                </Link>    
-                                <Link textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
-                                    border='2px solid #C668FF'
-                                    _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
-                                    onClick={() => setShowVideoTwo(true)}>
-                                    Demo
-                                </Link>
-                            </Box>
-                        </Box>
-                        {showVideoTwo ? <DemoVideoTwo closeDemoVideoTwo={closeDemoVideoTwo} /> : null}
-                    </Container> */}
 
                     <Container className="projectCard">
                         <Box borderRadius='20px'> 
@@ -146,17 +104,51 @@ export const Projects = ({ projectRef }) => {
                                     _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
                                     >Website
                                 </Link>    
-                                <Link textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
+                                {/* <Link textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
                                     border='2px solid #C668FF'
                                     _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
                                     onClick={() => setShowVideoThree(true)}>
                                     Demo
-                                </Link>
+                                </Link> */}
                             </Box>
                         </Box>
-                        {showVideoThree ? <DemoVideoThree closeDemoVideoThree={closeDemoVideoThree} /> : null}
+                        {/* {showVideoThree ? <DemoVideoThree closeDemoVideoThree={closeDemoVideoThree} /> : null} */}
                     </Container>
-            
+
+                    <Container className="projectCard">
+                        <Box borderRadius='20px'> 
+                            <Image borderRadius='20px 20px 0px 0px' minW={300} height={300} src="game-of-tic-tac-toe.png" alt="game-of-tic-tac-toe" />
+                        </Box>
+
+                        <Box mt={-4} p={10} className="projectButton">
+                            <Box className="projectDescBox">
+                                <Text className="projectDescText">Tic-Tac-Toe application developed using React and Redux where 2 users can play and get to know the final winner. 
+                                </Text>
+                            </Box>
+
+                            <Box display='flex' className="techStackBox" justifyContent='center'>
+                                <Text className="techStack">HTML</Text>
+                                <Text className="techStack">CSS</Text>
+                                <Text className="techStack">JS</Text>
+                                <Text className="techStack">React</Text>
+                                <Text className="techStack">Redux</Text>
+                            </Box>
+
+                            <Box className="buttonBox">
+                                <Link href="https://github.com/taherahmed14/game-of-tic-tac-toe" textDecoration='none' fontSize='14px' ml={20} color='#C668FF' p={8} 
+                                    border='2px solid #C668FF' target='_blank'
+                                    _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
+                                    >Code
+                                </Link>
+                                <Link href="https://game-of-tic-tac-toe-taherahmed14.vercel.app/" textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
+                                    border='2px solid #C668FF' target='_blank'
+                                    _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
+                                    >Website
+                                </Link>    
+                            </Box>
+                        </Box>
+                    </Container>
+
                 </SimpleGrid>
                 </Center>
                 </Container>
@@ -211,48 +203,6 @@ export const Projects = ({ projectRef }) => {
                             {/* {showVideoOne ? <DemoVideoOne closeDemoVideoOne={closeDemoVideoOne} /> : null} */}
                         </Container>
 
-                        {/* <Container className="projectCard">
-                            <Box borderRadius='20px'>
-                                <Image borderRadius='20px 20px 0px 0px' minW={300} height={300} src="groww-project.png" alt="adidas-project" />
-                            </Box>
-
-                            <Box mt={-4} p={10} className="projectButton">
-                                <Box className="projectDescBox">
-                                    <Text className="projectDescText">A full-stack clone of groww application developed by 6 members in time span of 5 days.
-                                        We have added features like filter products, view product graph, set SIP amount, OTP mail validation etc. 
-                                    </Text>
-                                </Box>
-
-                                <Box display='flex' className="techStackBox" justifyContent='center'>
-                                    <Text className="techStack">HTML</Text>
-                                    <Text className="techStack">CSS</Text>
-                                    <Text className="techStack">JS</Text>
-                                    <Text className="techStack">Node</Text>
-                                    <Text className="techStack">MongoDb</Text>
-                                </Box>
-
-                                <Box className="buttonBox">
-                                    <Link href="https://github.com/taherahmed14/Groww_Full-Stack_Project" textDecoration='none' fontSize='14px' ml={20} color='#C668FF' p={8} 
-                                        border='2px solid #C668FF' target='_blank'
-                                        _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
-                                        >Code
-                                    </Link>
-                                    <Link href="https://groww-project.herokuapp.com/home" textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
-                                        border='2px solid #C668FF' target='_blank'
-                                        _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
-                                        >Website
-                                    </Link>     */}
-                                    {/* <Link textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
-                                        border='2px solid #C668FF'
-                                        _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
-                                        onClick={() => setShowVideoTwo(true)}>
-                                        Demo
-                                    </Link> */}
-                                {/* </Box>
-                            </Box>
-                            {/* {showVideoTwo ? <DemoVideoTwo closeDemoVideoTwo={closeDemoVideoTwo} /> : null} */}
-                        {/* </Container> */}
-
                         <Container className="projectCard">
                             <Box borderRadius='20px'> 
                                 <Image borderRadius='20px 20px 0px 0px' minW={300} height={300} src="adidas-project.png" alt="adidas-project" />
@@ -292,6 +242,40 @@ export const Projects = ({ projectRef }) => {
                             </Box>
                             {/* {showVideoThree ? <DemoVideoThree closeDemoVideoThree={closeDemoVideoThree} /> : null} */}
                         </Container>
+
+                        <Container className="projectCard">
+                        <Box borderRadius='20px'> 
+                            <Image borderRadius='20px 20px 0px 0px' minW={300} height={300} src="game-of-tic-tac-toe.png" alt="game-of-tic-tac-toe" />
+                        </Box>
+
+                        <Box mt={-4} p={10} className="projectButton">
+                            <Box className="projectDescBox">
+                                <Text className="projectDescText">Tic-Tac-Toe application developed using React and Redux where 2 users can play and get to know the final winner. 
+                                </Text>
+                            </Box>
+
+                            <Box display='flex' className="techStackBox" justifyContent='center'>
+                                <Text className="techStack">HTML</Text>
+                                <Text className="techStack">CSS</Text>
+                                <Text className="techStack">JS</Text>
+                                <Text className="techStack">React</Text>
+                                <Text className="techStack">Redux</Text>
+                            </Box>
+
+                            <Box className="buttonBox">
+                                <Link href="https://github.com/taherahmed14/game-of-tic-tac-toe" textDecoration='none' fontSize='14px' ml={20} color='#C668FF' p={8} 
+                                    border='2px solid #C668FF' target='_blank'
+                                    _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
+                                    >Code
+                                </Link>
+                                <Link href="https://game-of-tic-tac-toe-taherahmed14.vercel.app/" textDecoration='none' fontSize='14px' minW='100px' ml={10} color='#C668FF' p={8} 
+                                    border='2px solid #C668FF' target='_blank'
+                                    _hover={{ cursor: 'pointer', background:'#C668FF', color:'black' }}
+                                    >Website
+                                </Link>    
+                            </Box>
+                        </Box>
+                    </Container>
                 
                     </SimpleGrid>
                     </Center>
